6e776c80d88e30f055f6a4c4f67a956b75595510
tkachenkaura <tkahenkaura@gmail.com>
2024-07-11 13:24:54 +0300

    Add files via upload

---
+++ task111111/task111111/interfaces/IList.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructuresLib.Interfaces
+{
+    public interface IList : ICollection
+    {
+        object this[int index] { get; set; }
+        void Insert(int index, object item);
+        void RemoveAt(int index);
+        int IndexOf(object item);
+    }
+}
